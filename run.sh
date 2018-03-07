278622b17af49c38c8580cf26d6ecbd7611e89cd
sverre320 <sverre@lunoe-nielsen.no>
2018-03-07 23:15:49 +0100

    Testing in parallell.

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 cd /home/sverre/sage/
-./sage -t ./src/sage/modules/fpmods/fpmods.py ./src/sage/modules/fpmods/fpmod_element.py ./src/sage/modules/fpmods/fpmod_morphism.py ./src/sage/modules/fpmods/fpmod_homspace.py
+./sage -tp 4 ./src/sage/modules/fpmods/resolutions.py
+./sage -tp 4 ./src/sage/modules/fpmods/fpmods.py ./src/sage/modules/fpmods/fpmod_element.py ./src/sage/modules/fpmods/fpmod_morphism.py ./src/sage/modules/fpmods/fpmod_homspace.py
 
